52321cc412991555f25cc95a4d4c2657491081d5
puneetkaura <pkaura@puneetkaura.com>
2023-05-25 18:37:38 +0000

    Adding CORS support

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ ACCESS_OPENAI = True
 
 DEFAULT_DICT = {'srcChain': 'Filecoin', 'destinationChain': 'Arbitrum', 'amount': 200, 'token': 'aUSDC'}
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Default root endpoint
 @app.get("/")
